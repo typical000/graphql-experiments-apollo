3a4b21817c609c2a98b929280b3349df52ea75b7
Pavel Davydov <typical000@gmail.com>
2018-02-01 17:34:18 +0200

    Fix load more for search results

--- src/components/User/UserList.js
+++ src/components/User/UserList.js
@@ -1,4 +1,4 @@
-import React, {Children} from 'react'
+import React, {Children, PureComponent} from 'react'
 import PropTypes from 'prop-types'
 import injectSheet from '../../utils/jss'
 import Loader from '../ui/Loader'
@@ -24,41 +24,56 @@ const styles = {
   },
 }
 
-const UserList = ({children, classes, loading, onLoadMoreClick}) => {
-  const showAction = !loading && onLoadMoreClick
+class UserList extends PureComponent {
+  static propTypes = {
+    classes: PropTypes.objectOf(PropTypes.string).isRequired,
+    children: PropTypes.node,
+    loading: PropTypes.bool,
+    onLoadMoreClick: PropTypes.func,
+  }
 
-  return (
-    <Loader transparent active={loading}>
-      <div className={classes.list}>
-        {Children.map(children, child => (
-          <div className={classes.item}>{child}</div>
-        ))}
-      </div>
-      {showAction && (
-        <div className={classes.action}>
-          <ButtonPrimary
-            onClick={() => {
-              onLoadMoreClick()
-            }}
-          >
-            Load more
-          </ButtonPrimary>
+  constructor(props) {
+    super(props)
+
+    this.defaultProps = {
+      children: null,
+      loading: false,
+      onLoadMoreClick: null,
+    }
+
+    this.handleLoadMoreClick = this.handleLoadMoreClick.bind(this)
+  }
+
+  /**
+   * Use as proxy method for passing updated count of rendered users
+   */
+  handleLoadMoreClick() {
+    this.props.onLoadMoreClick(this.props.children.length)
+  }
+
+  render() {
+    const {children, classes, loading, onLoadMoreClick} = this.props
+    const showAction = !loading && onLoadMoreClick
+
+    return (
+      <Loader transparent active={loading}>
+        <div className={classes.list}>
+          {Children.map(children, child => (
+            <div className={classes.item}>{child}</div>
+          ))}
         </div>
-      )}
-    </Loader>
-  )
-}
-
-UserList.propTypes = {
-  classes: PropTypes.objectOf(PropTypes.string).isRequired,
-  children: PropTypes.node,
-  loading: PropTypes.bool,
-  onLoadMoreClick: PropTypes.func,
-}
-
-UserList.defaultProps = {
-  loading: false,
-  onLoadMoreClick: null,
+        {showAction && (
+          <div className={classes.action}>
+            <ButtonPrimary
+              onClick={this.handleLoadMoreClick}
+            >
+              Load more
+            </ButtonPrimary>
+          </div>
+        )}
+      </Loader>
+    )
+  }
 }
 
 export default injectSheet(styles)(UserList)
--- src/containers/apollo/User/UserListWithData.js
+++ src/containers/apollo/User/UserListWithData.js
@@ -1,11 +1,9 @@
-import React, {PureComponent} from 'react'
+import React from 'react'
 import {Query} from 'react-apollo'
 import gql from 'graphql-tag'
 import User, {UserList} from '../../../components/User'
 
-// TODO: Remove unneded 'return' statements
-
-const ITEMS_PER_LOAD = 3 // TODO: Was 5
+const ITEMS_PER_LOAD = 5
 
 const QUERY = gql`
   query ($offset: Int!, $limit: Int!) {
@@ -29,58 +27,43 @@ const UserListWithData = () => (
       limit: ITEMS_PER_LOAD
     }}
   >
-    {({data, loading, fetchMore}) => {
-
-      // const offset = data && data.offsetUsers.length
-      // console.log('BEFORE ' + offset)
-
-      return (
-        <UserList
-          loading={loading}
-          onLoadMoreClick={() => {
-            /**
-             * We don't place handler in separated functon
-             * due to need to pass 'fetchMore' function
-             * as param to handler.
-             */
-            const offset = data.offsetUsers.length // TODO: Main problem goes here
-
-            // ==================
-            console.log(data)
-            // ==================
-
-            // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-            fetchMore({
-              variables: {
-                offset, // TODO: This value doesn't change in time
-                limit: ITEMS_PER_LOAD,
-              },
-              updateQuery: (prevResult, {fetchMoreResult}) => {
-                if (!fetchMoreResult) return prevResult
-                return Object.assign({}, prevResult, {
-                  offsetUsers: [
-                    ...prevResult.offsetUsers,
-                    ...fetchMoreResult.offsetUsers
-                  ]
-                })
-              }
-            })
-            // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-          }}
-        >
-          {data && data.offsetUsers.map(({id, avatar, screenname, gender, geo: {city}}) => (
-            <User
-              key={id}
-              avatar={avatar}
-              screenname={screenname}
-              gender={gender}
-              city={city}
-            />
-          ))}
-        </UserList>
-      )
-    }}
+    {({data, loading, fetchMore}) => (
+      <UserList
+        loading={loading}
+        onLoadMoreClick={(offset) => {
+          /**
+           * We don't place handler in separated functon
+           * due to need to pass 'fetchMore' function
+           * as param to handler.
+           */
+          fetchMore({
+            variables: {
+              offset,
+              limit: ITEMS_PER_LOAD,
+            },
+            updateQuery: (prevResult, {fetchMoreResult}) => {
+              if (!fetchMoreResult) return prevResult
+              return Object.assign({}, prevResult, {
+                offsetUsers: [
+                  ...prevResult.offsetUsers,
+                  ...fetchMoreResult.offsetUsers
+                ]
+              })
+            }
+          })
+        }}
+      >
+        {data && data.offsetUsers.map(({id, avatar, screenname, gender, geo: {city}}) => (
+          <User
+            key={id}
+            avatar={avatar}
+            screenname={screenname}
+            gender={gender}
+            city={city}
+          />
+        ))}
+      </UserList>
+    )}
   </Query>
 )
 
