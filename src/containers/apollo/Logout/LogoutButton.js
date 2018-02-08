0701ac79dfb75d6cdf1801db4baa5740aa0db068
Pavel Davydov <typical000@gmail.com>
2018-02-08 15:35:40 +0200

    Try to use Apollo consumer

--- src/containers/apollo/Logout/LogoutButton.js
+++ src/containers/apollo/Logout/LogoutButton.js
@@ -1,62 +1,65 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {withApollo} from 'react-apollo'
+import {ApolloConsumer} from 'react-apollo'
 import {ButtonPrimary} from '../../../components/ui/Button'
 import APP_DATA_FULL_QUERY from '../../../graphql/AppData/queries/fullData.graphql'
 import LOGOUT_MUTATION from '../../../graphql/Logout/mutations/logout.graphql'
 
-const LogoutButton = ({children, client: {mutate}}) => (
-  <ButtonPrimary
-    onClick={() => {
-      mutate({
-        mutation: LOGOUT_MUTATION,
+const LogoutButton = ({children}) => (
+  <ApolloConsumer>
+    {({mutate}) => (
+      <ButtonPrimary
+        onClick={() => {
+          mutate({
+            mutation: LOGOUT_MUTATION,
 
-        /**
-         * The esiest way - after mutation call 'refetchQueries' property
-         * that will cause more server calls for actualizing store state.
-         *
-         * But the most negative thing is that mutation already sends data
-         * that we need to change directly in cache.
-         * Anyway, if you want to try, uncomment this code:
-         *
-         * refetchQueries: [{query: APP_DATA_FULL_QUERY}]
-         *
-         * The more correct way is to update cache
-         * with your hande like here:
-         */
-        update: (proxy, {data: {logout}}) => {
-          const data = proxy.readQuery({query: APP_DATA_FULL_QUERY})
+            /**
+             * The esiest way - after mutation call 'refetchQueries' property
+             * that will cause more server calls for actualizing store state.
+             *
+             * But the most negative thing is that mutation already sends data
+             * that we need to change directly in cache.
+             * Anyway, if you want to try, uncomment this code:
+             *
+             * refetchQueries: [{query: APP_DATA_FULL_QUERY}]
+             *
+             * The more correct way is to update cache
+             * with your hande like here:
+             */
+            update: (proxy, {data: {logout}}) => {
+              const data = proxy.readQuery({query: APP_DATA_FULL_QUERY})
 
-          /**
-           * Yep, I'll like spread operators instead of this assign hell:
-           *
-           * data = Object.assign({}, data, {
-           *   appData: Object.assign({}, data.appData, {
-           *     guest: logout.guest
-           *   })
-           * })
-           */
-          proxy.writeQuery({
-            query: APP_DATA_FULL_QUERY,
-            data: {
-              ...data,
-              appData: {
-                ...data.appData,
-                guest: logout.guest
-              }
+              /**
+               * Yep, I'll like spread operators instead of this assign hell:
+               *
+               * data = Object.assign({}, data, {
+               *   appData: Object.assign({}, data.appData, {
+               *     guest: logout.guest
+               *   })
+               * })
+               */
+              proxy.writeQuery({
+                query: APP_DATA_FULL_QUERY,
+                data: {
+                  ...data,
+                  appData: {
+                    ...data.appData,
+                    guest: logout.guest
+                  }
+                }
+              })
             }
-          })
-        }
 
-      })
-    }}
-  >
-    {children}
-  </ButtonPrimary>
+          })
+        }}
+      >
+        {children}
+      </ButtonPrimary>
+    )}
+  </ApolloConsumer>
 )
 
 LogoutButton.propTypes = {
-  client: PropTypes.object.isRequired, // eslint-disable-line
   children: PropTypes.node,
 }
 
@@ -64,4 +67,4 @@ LogoutButton.defaultProps = {
   children: 'Logout'
 }
 
-export default withApollo(LogoutButton)
+export default LogoutButton
