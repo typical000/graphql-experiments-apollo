5a2640d74b5fdbcfffd3f73c80a57aab3a7cf365
Pavel Davydov <pavel.davydov@together.com>
2018-04-12 08:36:34 +0300

    Update dependencies. Rewrite button to Mutation render-prop

--- src/containers/Activity/FavoriteButton.js
+++ src/containers/Activity/FavoriteButton.js
@@ -1,39 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {withApollo} from 'react-apollo'
+import {Mutation} from 'react-apollo'
 import {Button} from '../../components/ui/Button'
 // eslint-disable-next-line
 import FAVORITE_MUTATION from '../../graphql/Activity/mutations/favorite.graphql'
 
-const FavoriteButton = ({children, active, userId, client: {mutate}}) => (
-  <Button
-    disabled={active}
-    onClick={() => {
-      mutate({
-        mutation: FAVORITE_MUTATION,
-        variables: {id: userId},
-
-        /**
-         * Instantly change UI with supposed data that we will receive from server later.
-         * This data will be passed to update method above.
-         */
-        optimisticResponse: {
-          favorite: {
-            id: userId,
-            active: true,
-            available: true,
-            __typename: 'Favorite',
-          },
-        },
-      })
+const FavoriteButton = ({children, active, userId}) => (
+  <Mutation
+    mutation={FAVORITE_MUTATION}
+    variables={{id: userId}}
+    optimisticResponse={{
+      favorite: {
+        id: userId,
+        active: true,
+        available: true,
+        __typename: 'Favorite',
+      },
     }}
   >
-    {children}
-  </Button>
+    {(addToFavorite, {loading}) => (
+      <Button disabled={active || loading} onClick={addToFavorite}>
+        {children}
+      </Button>
+    )}
+  </Mutation>
 )
 
 FavoriteButton.propTypes = {
-  client: PropTypes.object.isRequired, // eslint-disable-line
   children: PropTypes.node,
   active: PropTypes.bool,
   userId: PropTypes.string.isRequired,
@@ -44,4 +37,4 @@ FavoriteButton.defaultProps = {
   active: false,
 }
 
-export default withApollo(FavoriteButton)
+export default FavoriteButton
